935f96272ecec519f3c61ee79c65fed062c3176a
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-06-21 23:54:35 +0200

    Added: Site Config and Types

---
+++ config/dashboard.ts
@@ -0,0 +1,37 @@
+import { DashboardConfig } from "types"
+
+export const dashboardConfig: DashboardConfig = {
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs",
+    },
+    {
+      title: "Support",
+      href: "/support",
+      disabled: true,
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Overview",
+      href: "/dashboard",
+      icon: "post",
+    },
+    {
+      title: "Products",
+      href: "/dashboard/products",
+      icon: "post",
+    },
+    {
+      title: "Orders",
+      href: "/dashboard/orders",
+      icon: "billing",
+    },
+    {
+      title: "Settings",
+      href: "/dashboard/settings",
+      icon: "settings",
+    },
+  ],
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Digital Creators",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/",
+  },
+}
