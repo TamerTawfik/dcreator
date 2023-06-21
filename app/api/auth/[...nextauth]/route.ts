46d76a1cf0bcd5a06a2880bce9c22a9777353f4c
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-06-21 04:21:02 +0200

    Added: Google Auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,7 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import {db} from "@/lib/db";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
+import { authOptions } from "@/lib/auth"
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./navbar";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export default async function Nav() {
   const session = await getServerSession(authOptions);
