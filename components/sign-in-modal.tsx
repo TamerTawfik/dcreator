55a183612b7a49f60a1ea2ae0e17999ec48e66a7
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-06-22 05:56:49 +0200

    Added: A Logo to sign In Modal

--- components/sign-in-modal.tsx
+++ components/sign-in-modal.tsx
@@ -9,7 +9,7 @@ import {
 } from "react";
 import  Google  from "@/components/google";
 import LoadingDots from "@/components/loading-dots";
-import Image from "next/image";
+import { Icons } from "@/components/icons"
 
 const SignInModal = ({
   showSignInModal,
@@ -24,14 +24,8 @@ const SignInModal = ({
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
       <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
-            />
+          <a href="">
+          <Icons.logo />
           </a>
           <h3 className="font-display text-2xl font-bold">Sign In</h3>
           <p className="text-sm text-gray-500">
--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -53,7 +53,7 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
           onSelect={(event) => {
             event.preventDefault()
             signOut({
-              callbackUrl: `${window.location.origin}/login`,
+              callbackUrl: `${window.location.origin}/`,
             })
           }}
         >
