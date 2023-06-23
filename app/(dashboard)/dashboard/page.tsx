137c84ae45165f270acb9f3b1f0a2bd52c877750
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-06-23 21:45:33 +0200

    Added: Dashboard Overview Page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,62 +1,21 @@
-import { redirect } from "next/navigation"
 
-import { authOptions } from "@/lib/auth"
-import { db } from "@/lib/db"
-import { getCurrentUser } from "@/lib/session"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
 import { DashboardHeader } from "@/components/header"
-import { ProductCreateButton } from "@/components/product-create-button"
-import { ProductItem } from "@/components/product-item"
 import { DashboardShell } from "@/components/shell"
 
 export const metadata = {
   title: "Dashboard",
+  description: "Overview.",
 }
 
-export default async function DashboardPage() {
-  const user: any = await getCurrentUser()
-
-  if (!user) {
-    redirect(authOptions?.pages?.signIn || "/")
-  }
-
-  const products = await db.product.findMany({
-    where: {
-      authorId: user.id,
-    },
-    select: {
-      id: true,
-      name: true,
-      published: true,
-      createdAt: true,
-    },
-    orderBy: {
-      updatedAt: "desc",
-    },
-  })
-
+export default async function OrdersPage() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <ProductCreateButton />
-      </DashboardHeader>
-      <div>
-        {products?.length ? (
-          <div className="divide-y divide-border rounded-md border">
-            {products.map((product: any) => (
-              <ProductItem key={product.id} product={product} />
-            ))}
-          </div>
-        ) : (
-          <EmptyPlaceholder>
-            <EmptyPlaceholder.Icon name="post" />
-            <EmptyPlaceholder.Title>No products created</EmptyPlaceholder.Title>
-            <EmptyPlaceholder.Description>
-              You don&apos;t have any products yet. Start creating content.
-            </EmptyPlaceholder.Description>
-            <ProductCreateButton variant="outline" />
-          </EmptyPlaceholder>
-        )}
+      <DashboardHeader
+        heading="Overview"
+        text="Overview Dashboard."
+      />
+      <div className="grid gap-8">
+        
       </div>
     </DashboardShell>
   )
---
+++ app/(dashboard)/dashboard/products/page.tsx
@@ -0,0 +1,64 @@
+import { redirect } from "next/navigation"
+
+import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
+import { getCurrentUser } from "@/lib/session"
+import { EmptyPlaceholder } from "@/components/empty-placeholder"
+import { DashboardHeader } from "@/components/header"
+import { ProductCreateButton } from "@/components/product-create-button"
+import { ProductItem } from "@/components/product-item"
+import { DashboardShell } from "@/components/shell"
+
+export const metadata = {
+  title: "Products",
+  description: "Create and manage products.",
+}
+
+export default async function DashboardPage() {
+  const user: any = await getCurrentUser()
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/")
+  }
+
+  const products = await db.product.findMany({
+    where: {
+      authorId: user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+      published: true,
+      createdAt: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  })
+
+  return (
+    <DashboardShell>
+      <DashboardHeader heading="Products" text="Create and manage products.">
+        <ProductCreateButton />
+      </DashboardHeader>
+      <div>
+        {products?.length ? (
+          <div className="divide-y divide-border rounded-md border">
+            {products.map((product: any) => (
+              <ProductItem key={product.id} product={product} />
+            ))}
+          </div>
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon name="post" />
+            <EmptyPlaceholder.Title>No products created</EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              You don&apos;t have any products yet. Start creating content.
+            </EmptyPlaceholder.Description>
+            <ProductCreateButton variant="outline" />
+          </EmptyPlaceholder>
+        )}
+      </div>
+    </DashboardShell>
+  )
+}
