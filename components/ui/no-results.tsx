b330b854067c7eeb107c3e27b74f7646058e0f5e
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-08-04 02:38:32 +0200

    Added: Product List Component

---
+++ components/ui/no-results.tsx
@@ -0,0 +1,10 @@
+const NoResults = () => {
+    return ( 
+      <div className="flex items-center justify-center h-full w-full text-neutral-500">
+        No results found.
+      </div>
+     );
+  };
+   
+  export default NoResults;
+  
