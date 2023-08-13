666f2acab5c0d19917c5730aebb87136a4f8a414
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-08-14 00:44:02 +0200

    Removed: The Roles

--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link"
 import { User } from "next-auth"
 import { signOut } from "next-auth/react"
-import { getCurrentUser } from "@/lib/session"
 
 import {
   DropdownMenu,
@@ -18,12 +17,9 @@ interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
   user: Pick<User, "name" | "image" | "email">
 }
 
-export async function UserAccountNav({ user }: UserAccountNavProps) {
-  const currentUser = await getCurrentUser()
-
+export function UserAccountNav({ user }: UserAccountNavProps) {
   return (
     <>
-    {currentUser?.role == 'admin' ? 
     <DropdownMenu>
     <DropdownMenuTrigger>
       <UserAvatar
@@ -68,47 +64,7 @@ export async function UserAccountNav({ user }: UserAccountNavProps) {
         Sign out
       </DropdownMenuItem>
     </DropdownMenuContent>
-  </DropdownMenu> : 
-  <DropdownMenu>
-  <DropdownMenuTrigger>
-    <UserAvatar
-      user={{ name: user.name || null, image: user.image || null }}
-      className="h-8 w-8"
-    />
-  </DropdownMenuTrigger>
-  <DropdownMenuContent align="end">
-    <div className="flex items-center justify-start gap-2 p-2">
-      <div className="flex flex-col space-y-1 leading-none">
-        {user.name && <p className="font-medium">{user.name}</p>}
-        {user.email && (
-          <p className="w-[200px] truncate text-sm text-muted-foreground">
-            {user.email}
-          </p>
-        )}
-      </div>
-    </div>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem asChild>
-      <Link href="/profile">Profile</Link>
-    </DropdownMenuItem>
-    <DropdownMenuItem asChild>
-      <Link href="/profile/orders">My Orders</Link>
-    </DropdownMenuItem>
-    
-    <DropdownMenuSeparator />
-    <DropdownMenuItem
-      className="cursor-pointer"
-      onSelect={(event) => {
-        event.preventDefault()
-        signOut({
-          callbackUrl: `${window.location.origin}/`,
-        })
-      }}
-    >
-      Sign out
-    </DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>}
+  </DropdownMenu> 
     
     </>
   )
